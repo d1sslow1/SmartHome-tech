8f7bc2eb50cac262afaec8f5f7bd118ee2f2373c
d1sslow <artemdanielyan2006@yandex.ru>
2026-03-06 22:14:56 +0700

    Add startup logging to collector

--- telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CollectorApplication {
     public static void main(String[] args) {
+        System.out.println("=== COLLECTOR STARTING ===");
+        System.out.println("gRPC port: 59091");
         SpringApplication.run(CollectorApplication.class, args);
+        System.out.println("=== COLLECTOR STARTED ===");
     }
 }
