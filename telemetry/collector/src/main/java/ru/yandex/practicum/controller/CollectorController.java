dfac343e3b1e8525358f2013686baf0836c4f5c7
d1sslow <artemdanielyan2006@yandex.ru>
2026-03-06 23:06:48 +0700

    Add Kafka sending to CollectorController

--- telemetry/collector/src/main/java/ru/yandex/practicum/controller/CollectorController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/controller/CollectorController.java
@@ -2,29 +2,46 @@ package ru.yandex.practicum.controller;
 
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import ru.yandex.practicum.service.KafkaEventService;
 
+@Slf4j
 @GrpcService
+@RequiredArgsConstructor
 public class CollectorController extends CollectorControllerGrpc.CollectorControllerImplBase {
+
+    private final KafkaEventService kafkaEventService;
 
     @Override
     public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
-        System.out.println("Received HubEvent from hub: " + request.getHubId());
-        System.out.println("Event payload: " + request.getPayloadCase());
+        log.info("Received HubEvent from hub: {}", request.getHubId());
 
-        responseObserver.onNext(Empty.getDefaultInstance());
-        responseObserver.onCompleted();
+        try {
+            log.info("Successfully processed HubEvent");
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Error processing HubEvent", e);
+            responseObserver.onError(e);
+        }
     }
 
     @Override
     public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
-        System.out.println("Received SensorEvent from sensor: " + request.getId());
-        System.out.println("Hub ID: " + request.getHubId());
-        System.out.println("Event payload: " + request.getPayloadCase());
-        responseObserver.onNext(Empty.getDefaultInstance());
-        responseObserver.onCompleted();
+        log.info("Received SensorEvent from sensor: {}", request.getId());
+
+        try {
+            log.info("Successfully processed SensorEvent");
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Error processing SensorEvent", e);
+            responseObserver.onError(e);
+        }
     }
 }
