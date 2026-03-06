cd1d4c6a4ece8772ed258c79dfb75f974c02c81a
d1sslow <artemdanielyan2006@yandex.ru>
2026-03-06 22:24:16 +0700

    Fix collector gRPC configuration and add startup logging

--- telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
@@ -2,13 +2,24 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CollectorApplication {
     public static void main(String[] args) {
         System.out.println("=== COLLECTOR STARTING ===");
-        System.out.println("gRPC port: 59091");
-        SpringApplication.run(CollectorApplication.class, args);
-        System.out.println("=== COLLECTOR STARTED ===");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+
+        try {
+            ConfigurableApplicationContext context = SpringApplication.run(CollectorApplication.class, args);
+            System.out.println("=== COLLECTOR STARTED SUCCESSFULLY ===");
+
+            System.out.println("Active profiles: " + String.join(", ", context.getEnvironment().getActiveProfiles()));
+        } catch (Exception e) {
+            System.out.println("=== COLLECTOR FAILED TO START ===");
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
