62febcc423b7bcca58af2cc498e0096708dde9bf
d1sslow <artemdanielyan2006@yandex.ru>
2026-03-07 00:20:24 +0700

    Add comprehensive startup logging

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
@@ -11,41 +11,30 @@ import ru.yandex.practicum.processor.SnapshotProcessor;
 @ConfigurationPropertiesScan
 public class AnalyzerApplication {
     public static void main(String[] args) {
-        System.out.println("ANALYZER STARTING...");
-        System.out.println("Current directory: " + System.getProperty("user.dir"));
-        System.out.println("Java version: " + System.getProperty("java.version"));
-        System.out.flush();
+        System.out.println("=== ANALYZER STARTING ===");
+
+        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
+
+        System.out.println("=== CONTEXT LOADED ===");
+        System.out.println("Beans count: " + context.getBeanDefinitionCount());
 
         try {
-            ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
-            System.out.println("ANALYZER CONTEXT LOADED");
-            System.out.println("Active beans count: " + context.getBeanDefinitionCount());
-            System.out.flush();
-
-            System.out.println("Getting HubEventProcessor bean...");
             HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
-            System.out.println("Got HubEventProcessor: " + hubEventProcessor);
+            System.out.println("HubEventProcessor found: " + hubEventProcessor);
 
-            System.out.println("Getting SnapshotProcessor bean...");
             SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
-            System.out.println("Got SnapshotProcessor: " + snapshotProcessor);
-            System.out.flush();
+            System.out.println("SnapshotProcessor found: " + snapshotProcessor);
 
-            System.out.println("Starting HubEventProcessor thread...");
             Thread hubEventsThread = new Thread(hubEventProcessor);
             hubEventsThread.setName("HubEventHandlerThread");
             hubEventsThread.start();
             System.out.println("HubEventProcessor thread started");
-            System.out.flush();
 
-            System.out.println("Calling snapshotProcessor.start()...");
             snapshotProcessor.start();
 
         } catch (Exception e) {
-            System.err.println("ANALYZER FAILED TO START");
+            System.err.println("ERROR: " + e.getMessage());
             e.printStackTrace();
-            System.err.flush();
-            throw e;
         }
     }
 }
