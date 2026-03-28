4119323d6b86d710dc711b0e693891d423023d4d
d1sslow <artemdanielyan2006@yandex.ru>
2026-03-29 00:40:54 +0700

    discovery server

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
@@ -3,23 +3,24 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.yandex.practicum.processor.HubEventProcessor;
-import ru.yandex.practicum.processor.SnapshotProcessor;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class AnalyzerApplication {
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
+        System.out.println("=== AnalyzerApplication main() ===");
+        System.out.println("Current directory: " + System.getProperty("user.dir"));
+        System.out.println("Java version: " + System.getProperty("java.version"));
 
-        HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
-        SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
-
-        Thread hubEventsThread = new Thread(hubEventProcessor);
-        hubEventsThread.setName("HubEventHandlerThread");
-        hubEventsThread.start();
-
-        snapshotProcessor.start();
+        try {
+            SpringApplication.run(AnalyzerApplication.class, args);
+        } catch (Exception e) {
+            System.err.println("=== AnalyzerApplication failed ===");
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
--- telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
@@ -2,10 +2,26 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class CollectorApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(CollectorApplication.class, args);
+        System.out.println("=== COLLECTOR STARTING ===");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Working directory: " + System.getProperty("user.dir"));
+
+        try {
+            SpringApplication.run(CollectorApplication.class, args);
+            System.out.println("=== COLLECTOR STARTED SUCCESSFULLY ===");
+        } catch (Exception e) {
+            System.out.println("=== COLLECTOR FAILED TO START ===");
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
