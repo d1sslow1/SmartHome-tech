2471daeb92343d2bb1cbb8560d401d39989a5d5b
d1sslow <artemdanielyan2006@yandex.ru>
2026-03-29 00:52:04 +0700

    merge: resolve conflicts with develop

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.processor.HubEventProcessor;
+import ru.yandex.practicum.processor.SnapshotProcessor;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -16,7 +19,33 @@ public class AnalyzerApplication {
         System.out.println("Java version: " + System.getProperty("java.version"));
 
         try {
-            SpringApplication.run(AnalyzerApplication.class, args);
+            ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
+            System.out.println("=== Spring context loaded ===");
+            System.out.println("Beans count: " + context.getBeanDefinitionCount());
+
+            String[] beanNames = context.getBeanDefinitionNames();
+            System.out.println("First 10 beans:");
+            for (int i = 0; i < Math.min(10, beanNames.length); i++) {
+                System.out.println("  - " + beanNames[i]);
+            }
+
+            System.out.println("Getting HubEventProcessor bean...");
+            HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
+            System.out.println("HubEventProcessor: " + hubEventProcessor);
+
+            System.out.println("Getting SnapshotProcessor bean...");
+            SnapshotProcessor snapshotProcessor = context.getBean(SnapshotProcessor.class);
+            System.out.println("SnapshotProcessor: " + snapshotProcessor);
+
+            System.out.println("Starting HubEventProcessor thread...");
+            Thread hubEventsThread = new Thread(hubEventProcessor);
+            hubEventsThread.setName("HubEventHandlerThread");
+            hubEventsThread.start();
+            System.out.println("HubEventProcessor thread started");
+
+            System.out.println("Starting SnapshotProcessor...");
+            snapshotProcessor.start();
+
         } catch (Exception e) {
             System.err.println("=== AnalyzerApplication failed ===");
             e.printStackTrace();
--- telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -16,8 +17,9 @@ public class CollectorApplication {
         System.out.println("Working directory: " + System.getProperty("user.dir"));
 
         try {
-            SpringApplication.run(CollectorApplication.class, args);
+            ConfigurableApplicationContext context = SpringApplication.run(CollectorApplication.class, args);
             System.out.println("=== COLLECTOR STARTED SUCCESSFULLY ===");
+            System.out.println("Active profiles: " + String.join(", ", context.getEnvironment().getActiveProfiles()));
         } catch (Exception e) {
             System.out.println("=== COLLECTOR FAILED TO START ===");
             e.printStackTrace();
