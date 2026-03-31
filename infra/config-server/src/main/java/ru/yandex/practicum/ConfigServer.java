8849e1c7bc460d6e7bcb7e4583f1045f1f9020aa
d1sslow <artemdanielyan2006@yandex.ru>
2026-04-01 03:27:18 +0700

    fix: add missing config files and fix discovery/config servers

--- infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
+++ infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
@@ -2,16 +2,14 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigServer {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
     }
-
 }
