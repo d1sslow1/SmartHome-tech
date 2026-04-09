6c1ce6f7d3f629b75aa1e14a31b18c07848edc6f
d1sslow <artemdanielyan2006@yandex.ru>
2026-04-10 02:36:07 +0700

    fix: add PostgreSQL driver for shopping-store and shopping-cart

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/exception/ProductNotAvailableException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.exception;
-
-public class ProductNotAvailableException extends RuntimeException {
-
-    public ProductNotAvailableException(String message) {
-        super(message);
-    }
-}
